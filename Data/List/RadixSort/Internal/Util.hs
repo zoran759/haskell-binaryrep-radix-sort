9d56dc35fec51e479fd82fe46863b5e8712fac9c
Zoran Rasic <zokiupwork@gmail.com>
2013-03-07 20:41:45 +0100

    save product

--- Data/List/RadixSort/Internal/Util.hs
+++ Data/List/RadixSort/Internal/Util.hs
@@ -48,27 +48,31 @@ partBySign poss negs (x:xs) = if isNeg x
 ------------------------------------------
 
 partListByDigit :: (RadixRep a) => SortInfo -> Int -> MVector s (Seq a) -> [a] -> ST s ()
-partListByDigit _sortInfo _digit _vec []  = return ()
-partListByDigit sortInfo digit vec (x:xs) = do
+partListByDigit sortInfo' digit' vec' list = do
+        partListByDigitR bitsToShift' sortInfo' digit' vec' list
+  where      
+    bitsToShift' = digit' * (sortInfo' .$ siDigitSize)
+    
+    partListByDigitR _bitsToShift _sortInfo _digit _vec []  = return ()
+    partListByDigitR bitsToShift sortInfo digit vec (x:xs) = do
         s <- VM.read vec digitVal
         VM.write vec digitVal (s S.|> x)
-        partListByDigit sortInfo digit vec xs
+        partListByDigitR bitsToShift sortInfo digit vec xs
         return ()
-  where
-    digitVal = case sizeOf x of
-                        64 -> wordGetDigitVal sortInfo signedQ digit $ (toWordRep x :: Word64)
-                        32 -> wordGetDigitVal sortInfo signedQ digit $ (toWordRep x :: Word32)
-                        16 -> wordGetDigitVal sortInfo signedQ digit $ (toWordRep x :: Word16)
-                        8 -> wordGetDigitVal sortInfo signedQ digit $ (toWordRep x :: Word8)
+      where
+        digitVal = case sizeOf x of
+                        64 -> wordGetDigitVal bitsToShift sortInfo signedQ digit $ (toWordRep x :: Word64)
+                        32 -> wordGetDigitVal bitsToShift sortInfo signedQ digit $ (toWordRep x :: Word32)
+                        16 -> wordGetDigitVal bitsToShift sortInfo signedQ digit $ (toWordRep x :: Word16)
+                        8 -> wordGetDigitVal bitsToShift sortInfo signedQ digit $ (toWordRep x :: Word8)
                         other -> error $ printf "size %d not supported!" other
 
-    signedQ = signedQual x
-    
+        signedQ = signedQual x
 
 ------------------------------------------
 
-wordGetDigitVal :: (Bits a, Integral a) => SortInfo -> SignedQual -> Int -> a -> Int
-wordGetDigitVal sortInfo signed digit bits =
+wordGetDigitVal :: (Bits a, Integral a) => Int -> SortInfo -> SignedQual -> Int -> a -> Int
+wordGetDigitVal bitsToShift sortInfo signed digit bits =
       assert (digit >= 0 && digit <= topDigit) $ fromIntegral digitVal
     where
       digitVal = shiftR (bits .&. mask) bitsToShift
@@ -77,7 +81,6 @@ wordGetDigitVal sortInfo signed digit bits =
               then shiftL (fromIntegral digitMaskSignExcl) bitsToShift
               else shiftL (fromIntegral digitMask) bitsToShift
               
-      bitsToShift = digit * bitsPerDigit
       digitMask = bit bitsPerDigit -1 :: Word
       digitMaskSignExcl = (bit (bitsPerDigit-1) -1) :: Word   -- sign excluded
       bitsPerDigit = sortInfo .$ siDigitSize
@@ -91,19 +94,16 @@ collectVecToDList vec n dl =
            then new_accum_dl
            else collectVecToDList vec (n-1) new_accum_dl
       where
-        dlFromSeq s = D.fromList $ F.toList s
         new_accum_dl = dln `D.append` dl
-        dln = dlFromSeq $ vec V.! n
+        dln = D.fromList $ F.toList $ vec V.! n
 
 ------------------------------------------
 
 calcDigitSize :: [a] -> Int
-calcDigitSize list =
-  let (_prefix, postfix1) = L.splitAt 16 list
-  in if null postfix1
-     then 2  -- use shorter vectors
-     else let (_prefix, postfix2) = L.splitAt 500 postfix1
-          in if null postfix2
-                then 4  -- use small vectors
+calcDigitSize _list = 8
+  {-
+        let (_prefix, postfix) = L.splitAt 500 list
+        in if null postfix
+                then 4  -- use small vectors (save space)
                 else 8  -- use bigger vectors
-                        
+                -}
