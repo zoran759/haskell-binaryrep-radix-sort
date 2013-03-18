5c191c00fe63c71b0a5cbbc0bee6ecbd897250dc
Zoran Rasic <zokiupwork@gmail.com>
2013-03-18 11:55:47 +0100

    lsd set apart initial digit code

--- Data/List/RadixSort/Internal/LSD.hs
+++ Data/List/RadixSort/Internal/LSD.hs
@@ -25,17 +25,10 @@ lsdRadixSort _indexMap _sortInfo _digitsConstancy [] = D.empty
 lsdRadixSort _indexMap _sortInfo _digitsConstancy [x] = D.singleton x
 lsdRadixSort indexMap sortInfo @ SortInfo {..} digitsConstancy list@(x:_) =
         assert (sizeOf (indexMap x) `mod` siDigitSize == 0) $ runST $ do
-        
+                
         vecIni <- VM.replicate (siTopDigitVal+1) S.empty
-        -- partition by digit 0
-        let digit' = 0
-        if not $ digitsConstancy V.! digit'
-          then partListByDigit indexMap sortInfo digit' vecIni list
-          else do -- constant data on digit 0, write list to digitVal pos.
-                 let bitsToShift = 0
-                     digitVal = getDigitVal sortInfo (indexMap x) digit' bitsToShift
-                 VM.write vecIni digitVal $ S.fromList list    
         
+        digitPassIni vecIni
         refVecFrom <- newSTRef vecIni
 
         M.when (siTopDigit > 0) $
@@ -45,7 +38,18 @@ lsdRadixSort indexMap sortInfo @ SortInfo {..} digitsConstancy list@(x:_) =
         readSTRef refVecFrom >>= V.unsafeFreeze >>= (return . collectVecToDList siTopDigitVal D.empty)
 
   where
-    digitPass digit refVecFrom = do -- sort by digit
+    -- partition by digit 0      
+    digitPassIni vecIni = do  
+        let digit = 0
+        if not $ digitsConstancy V.! digit
+          then partListByDigit indexMap sortInfo digit vecIni list
+          else do -- constant data on digit 0, write list to digitVal pos.
+                 let bitsToShift = 0
+                     digitVal = getDigitVal sortInfo (indexMap x) digit bitsToShift
+                 VM.write vecIni digitVal $ S.fromList list
+                 
+    -- sort by digit        
+    digitPass digit refVecFrom = do 
                 vecFrom <- readSTRef refVecFrom
                 vecTo <- VM.replicate (siTopDigitVal+1) S.empty
                 
